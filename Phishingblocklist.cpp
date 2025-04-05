32e2703015d53c76e0720f565ed528788c16584e
Brian Doyle <briandoyle@brians-mbp-64.myfiosgateway.com>
2025-04-05 12:45:24 -0400

    I finished the code just debugging and found the error on line 43 of the .cpp file so it should work now

--- Phishingblocklist.cpp
+++ Phishingblocklist.cpp
@@ -6,35 +6,41 @@
 #include "Phishingblocklist.h"
 using namespace std;
 
+// Constructor will Initialize the capacity and allocate memory 
 PhishingBlocklist::PhishingBlocklist(){
-    capacity = 20;
+    capacity = 10;
     size = 0;
     emails = new string[capacity];
 }
-
+// Destructor Will free up memory when the function scope has passed
 PhishingBlocklist::~PhishingBlocklist(){
     delete[] emails;
 }
 
+// This will double the size of the array and copy all the old emails into the new array with double the capacity 
 void PhishingBlocklist::resize(){
     capacity *= 2;
     std::string* newEmails = new std::string[capacity];
+    // This loop is responible for copying over the old emails to the new array 
     for(int i=0; i < size; i++){
         newEmails[i] = emails[i];
     }
+    // This updats the pointer and delets the old array
     delete[] emails;
     emails = newEmails;
 }
 
 bool PhishingBlocklist::addEmail(const std::string& email){
+    // This if statement checks for duplicates in the list
     if(contains(email)){
         return false;
     }
+    // This statment will check the size of the array and resize if needed
     if(size == capacity){
         resize();
     }
     emails[size++] = email;
-    return false;
+    return true;
 }
 
 bool PhishingBlocklist::removeEmail(const std::string& email){
@@ -44,15 +50,17 @@ bool PhishingBlocklist::removeEmail(const std::string& email){
                 // shift j to the left 
                 emails[j] = emails[j + 1];
             }
-            size--;
+            --size;
             return true;
         }
     }
+    // if email is not found in the list it will return false
     return false;
 }
 
+// Checks if an email is in the list 
 bool PhishingBlocklist::contains(const std::string& email)const{
-    for(int i=0; i < size; i++){
+    for(int i = 0; i < size; i++){
         if(emails[i] == email) {
             return true;
         }
@@ -60,6 +68,7 @@ bool PhishingBlocklist::contains(const std::string& email)const{
     return false;
 }
 
+// prints all emails in the current list
 void PhishingBlocklist::printBlocklist() const{
     if(size == 0){
         std::cout << "List is empty" << endl;
